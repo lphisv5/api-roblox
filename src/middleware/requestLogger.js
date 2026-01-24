03f81ab55994cff9bbd0a1715268e0767ebffd23
YANZ <165420181+lphisv5@users.noreply.github.com>
2026-01-24 17:52:55 +0700

    Create requestLogger.js

---
+++ src/middleware/requestLogger.js
@@ -0,0 +1,21 @@
+import { logger } from '../utils/logger.js';
+
+export const requestLogger = (req, res, next) => {
+  const start = Date.now();
+
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    const level = res.statusCode >= 400 ? 'warn' : 'http';
+
+    logger[level]('HTTP Request', {
+      method: req.method,
+      path: req.path,
+      statusCode: res.statusCode,
+      duration,
+      ip: req.ip,
+      userAgent: req.get('user-agent')
+    });
+  });
+
+  next();
+};
